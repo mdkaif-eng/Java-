abd517a5a53d9deeead4d66b1ecf7590004ad67d
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-20 19:57:31 +0530

    variable can not use directly

---
+++ function day6/methodscope.java
@@ -0,0 +1,10 @@
+public class methodscope{
+    public static void printS(){
+        int s = 465;
+
+    }
+    public static void main(String[]args){
+    System.out.println(S);
+}
+}
+//we can not use variable directly ,we can use variable as a paramater 
