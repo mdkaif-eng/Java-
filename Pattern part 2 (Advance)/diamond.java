ab57d1aa0ca1394fadab4d921f7887ec6ad773e9
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-23 17:21:45 +0530

    Diamond

---
+++ Pattern part 2 (Advance)/diamond.java
@@ -0,0 +1,36 @@
+public class diamond{
+    public static void Diamond(int n){
+        //1st Half
+         for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+
+            }
+            // star
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");            
+         }
+         System.out.println();
+    }
+
+    //2nd Half
+    for(int i=n; i>=1; i--){
+
+            //spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+
+            }
+            // star
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");            
+         }
+         System.out.println();
+    }
+
+    }
+    public static void main(String[]args){
+        Diamond(4);
+    }
+}
