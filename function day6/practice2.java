b30fadf2dbcd7fd419d935217c0815ac500d2b6e
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-24 22:27:58 +0530

    practice2

--- function day6/practice2.java
+++ function day6/practice2.java
@@ -1,5 +1,28 @@
+//write a method named isEven that accepts an int argument. The method should return true if even, or false.Also write a program to test your method.
+
+import java.util.*;
+
 public class practice2{
     public static void main(String[]args){
-        
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("Enter an integar: ");
+        num = sc.nextInt();
+
+        if(isEven(num)){
+            System.out.println("Number is even: ");
+        }
+        else{
+            System.out.println("Number is odd: ");
+        }
+       
+    }
+    public static boolean isEven(int number){
+        if(number == 2 % 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
